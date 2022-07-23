2ecbdd243234bdf6437a5f9abed1a48a851b4d82
Antônio Pinheiro <tony@debian-BULLSEYE-live-builder-AMD64>
2022-07-23 10:35:29 -0300

    First Commit

---
+++ install_database.sh
@@ -0,0 +1,77 @@
+#http://repo.mongodb.org/apt/debian/dists/bullseye/mongodb-org/6.0/main/binary-amd64/
+
+install_mongodb(){
+    
+    echo
+    echo "Installing mongoDB"
+    echo
+    wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | apt-key add - echo "deb http://repo.mongodb.org/apt/debian bullseye/mongodb-org/6.0 main" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+    apt-get update -y
+    if  apt-get install mongodb-org -y; then
+        echo "mongoDB successfuly installed..."
+        echo
+        echo "Configuring mongoDB..."
+        echo
+        main_function
+    else
+        echo "Installation failed, please try again..."
+        main_function
+    fi
+}
+
+install_mariadb(){
+    
+    echo
+    echo "Installing MySQL / MariaDB"
+    echo
+    apt-get update -y
+    if  apt-get install mariadb-server -y; then
+        echo "MySQL / MariaDB successfuly installed..."
+        main_function
+    else
+        echo "Installation failed, please try again..."
+        main_function
+    fi
+}
+
+install_postgresql(){
+    
+    echo
+    echo "Installing MySQL / MariaDB"
+    echo
+    apt-get update -y
+    if  sudo apt install postgresql postgresql-contrib -y; then
+        echo "PostgreSQL successfuly installed..."
+        main_function
+    else
+        echo "Installation failed, please try again..."
+        main_function
+    fi
+}
+
+
+main_function(){
+echo
+echo "Select an option: "
+echo
+echo "1 - Install mongoDB"
+echo "2 - Install MySql / MariaDB"
+echo "3 - Install PostgreSQL"
+echo "4 - Exit"
+echo
+
+while :
+do
+  read select_option
+  case $select_option in
+
+    1)  install_mongodb;;
+    2)  install_mysql;;
+    3)  install_postgresql;;
+    4)  exit
+
+  esac
+done
+}
+
+main_function
